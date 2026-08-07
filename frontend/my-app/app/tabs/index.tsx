01eee1b0e5ab634d67ef515d3eee787a24f126b6
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-08-07 11:56:00 +0300

    custom color initial palette and pretty menu

--- frontend/my-app/app/tabs/index.tsx
+++ frontend/my-app/app/tabs/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Home() {
     return(
-        <View>
+        <View className = "flex flex-grow bg-slate-200">
             <Text> Home page </Text>
         </View>
     );
---
+++ frontend/my-app/constants/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  white: "#fff",
+  beigeDark: "#ed1e1e",
+  beigeLight: "#f4e8e8",
+  pinkLight: "#f9c7dd",
+  pinkMedium: "#ffb5d2",
+  pinkDark: "#ff7fb4",
+};
