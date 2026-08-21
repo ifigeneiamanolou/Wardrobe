f68319c26599be70d4124c519885268f33c00928
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-08-21 12:29:39 +0300

    Basic server creation with uvicorn

--- backend/src/routes/main.py
+++ backend/src/routes/main.py
@@ -23,6 +23,10 @@ app.add_middleware(
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host = "0.0.0.0", port = "8000")
+
 
 
 
