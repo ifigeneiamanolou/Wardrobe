b63ebb8c6497166444139454a2d5a816f24f04c7
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-08-22 12:44:37 +0300

    Context manager for sign in and sign out logic

---
+++ frontend/my-app/src/ctx.tsx
@@ -0,0 +1,42 @@
+import {use, createContext, type PropsWithChildren} from 'react';
+import { useStorageState } from './hooks/useStorageState';
+import React from 'react';
+
+// Context provider
+const AuthContext = createContext<{
+    signIn: () => void;
+    signOut : () => void;
+    isLoading : boolean;
+    session : string | null;
+} | null>(null);
+
+// Use this to access user information
+export function useSession(){
+    const value = use(AuthContext);
+    if(!value){
+        new Error('use session must be wrapped in a session provider');
+    };
+    return value;
+}
+
+// Wrapper around react components
+export function SessionProvider({children} : PropsWithChildren){
+    const [[isLoading, session], setSession] = useStorageState('session');      // session is our key
+
+    return(
+        // Provide context to the childer of this parent element
+        <AuthContext.Provider 
+            value = {{
+                signIn : () => {
+                    setSession('x');        // log in logic here
+                },
+                signOut : () => {
+                    setSession(null);
+                },
+                isLoading,
+                session
+            }}>  
+            {children}
+        </AuthContext.Provider>
+    );
+}
