1245ff07dcc9503f0982000980aaaa1998910224
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-08-23 20:55:55 +0300

    Added animation for the splash screen

--- frontend/my-app/src/components/bouncingAnimation.tsx
+++ frontend/my-app/src/components/bouncingAnimation.tsx
@@ -1,34 +1,127 @@
 import React, { useEffect } from "react";
 import Animated, {
   useSharedValue,
-  useAnimatedStyle,
+  withRepeat,
+  withSequence,
   withTiming,
-  withSpring,
+  useAnimatedStyle,
+  cancelAnimation,
+  ReduceMotion,
+  Easing
 } from "react-native-reanimated";
-
-import { View } from "react-native";
-
-// Shared values live on the UI thread, run on 60fps, and don't trigger react rerenders
-const location = useSharedValue(1);  
-
-const animatedBounce = useAnimatedStyle(() => ({
-
-}));
+import { View, } from "react-native";
+import {useSession} from '../ctx';
 
 export default function AnimatedBag(){
-    // Shared values (animation state)
+    // Shared values live on the UI thread, run on 60fps, and don't trigger react rerenders
+    const translateY = useSharedValue(0);
+    const scaleX = useSharedValue(0.8);
+    const scaleY = useSharedValue(1.2);
 
-    // Trigger animation on mount
+    // Trigger a repeated animation sequence on mount
     useEffect(() => {
+        // Animate the vertical position of the ball
+        translateY.value = withRepeat(
+            withSequence(
+                withTiming(-200, {
+                    duration : 300,
+                    easing : Easing.out(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(0, {
+                    duration : 300,
+                    easing : Easing.in(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                })
+            ),
+            -1,
+            false
+        );
 
+        // Animate the horizontal deformation of the ball
+        scaleX.value = withRepeat(
+            withSequence(
+                withTiming(1, {
+                    duration : 150,
+                    easing : Easing.inOut(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(0.8, {
+                    duration : 450,
+                    easing : Easing.in(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(1.3, {
+                    duration : 150,
+                    easing : Easing.out(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(0.8, {
+                    duration : 150,
+                    easing : Easing.inOut(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                })
+            ),
+            -1,
+            false
+        );
+
+        // Animate the vertical deformation of the ball
+        scaleY.value = withRepeat(
+            withSequence(
+                withTiming(1, {
+                    duration : 150,
+                    easing : Easing.inOut(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(1.2, {
+                    duration : 450,
+                    easing : Easing.in(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(0.8, {
+                    duration : 150,
+                    easing : Easing.out(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                }),
+                withTiming(1.2, {
+                    duration : 150,
+                    easing : Easing.inOut(Easing.cubic),
+                    reduceMotion : ReduceMotion.System
+                })
+            ),
+            -1,
+            false
+        );
+
+        return () => {
+            cancelAnimation(translateY);
+            cancelAnimation(scaleX);
+            cancelAnimation(scaleY);
+        };
     }, []);
 
+    // Create an animation stylesheet for the image
+    const stylesheet = useAnimatedStyle(() => ({
+        transform : [
+            {translateY : translateY.value},
+            {scaleX : scaleX.value},
+            {scaleY : scaleY.value}
+        ]
+    }))
+
     // Stop the animation
+    const session = useSession();
+    if(!session?.isLoading){
+        cancelAnimation(translateY);
+        cancelAnimation(scaleX);
+        cancelAnimation(scaleY);
+    }
 
     return(
         <View className = "flex-1 justify-center align-middle">
-            <Animated.Image 
-                source = {{uri : "../../assets/animations/layerCopy.png"}}
+            <Animated.Image style = {stylesheet}
+                source = {require("../../assets/animations/layerCopy.png")}
             />
         </View>
     )
---
+++ frontend/my-app/src/components/splash.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Text, View } from "react-native"
+export default function Splash(){
+    return(
+        <View>
+            <Text> Loading</Text>
+        </View>
+    )
+}
